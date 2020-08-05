96aa9fd38a5e6879d548f404f0056be6e724d3ed
AntonioGSC <antoniogabrielagsc@gmail.com>
2020-08-05 13:05:01 -0300

    Teacher-list

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './styles.css';
+import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars0.githubusercontent.com/u/61213281?s=460&u=c91beb60500835329fef881ade85f046273b802f&v=4"
+          alt="Antonio Gabriel"
+        />
+        <div>
+          <strong>Antonio Gabriel</strong>
+          <span>Programação</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br />
+        <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsAppIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
